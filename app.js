2af87d6f42e98dbf1fd25fe865d6b491b46fe79c
ldevernay <boblehamster@yahoo.fr>
2017-03-13 16:41:16 +0100

    answer

--- app.js
+++ app.js
@@ -24,7 +24,25 @@ app.get("/webhook", function (req, res) {
   }
 });
 
-// All callbacks for Messenger will be POST-ed here
+// // All callbacks for Messenger will be POST-ed here
+// app.post("/webhook", function (req, res) {
+//   // Make sure this is a page subscription
+//   if (req.body.object == "page") {
+//     // Iterate over each entry
+//     // There may be multiple entries if batched
+//     req.body.entry.forEach(function(entry) {
+//       // Iterate over each messaging event
+//       entry.messaging.forEach(function(event) {
+//         if (event.postback) {
+//           processPostback(event);
+//         }
+//       });
+//     });
+//
+//     res.sendStatus(200);
+//   }
+// });
+
 app.post("/webhook", function (req, res) {
   // Make sure this is a page subscription
   if (req.body.object == "page") {
@@ -35,6 +53,8 @@ app.post("/webhook", function (req, res) {
       entry.messaging.forEach(function(event) {
         if (event.postback) {
           processPostback(event);
+        } else if (event.message) {
+          processMessage(event);
         }
       });
     });
@@ -42,6 +62,24 @@ app.post("/webhook", function (req, res) {
     res.sendStatus(200);
   }
 });
+
+function processMessage(event) {
+  if (!event.message.is_echo) {
+    var message = event.message;
+    var senderId = event.sender.id;
+
+    console.log("Received message from senderId: " + senderId);
+    console.log("Message is: " + JSON.stringify(message));
+
+    // You may get a text or attachment but not both
+    if (message.text) {
+      sendMessage(senderId, {text: "Google!"});
+    } else if (message.attachments) {
+      sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+    }
+  }
+}
+
 
 function processPostback(event) {
   var senderId = event.sender.id;
